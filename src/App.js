fc5e1e20ab54d070e594d725baaf5bb606db2e42
Shubham Verma <shubhamverma@Shubhams-Mac-mini.local>
2024-06-11 16:49:18 +0530

    Added ag chart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import 'react-quill/dist/quill.snow.css';
 import { LineChart } from '@mui/x-charts/LineChart';
 import { Quill } from 'react-quill';
 import {pageHitsData} from './data'
-
+import { AgChartsReact } from 'ag-charts-react';
+import {produce} from 'immer'
 console.log(pageHitsData)
 
 const emailTypes = {
@@ -36,6 +37,58 @@ function App() {
   const [formattedGraphData, setFormmatedGraphData]= useState({})
   const [checkedCourseIds, setCheckedCourseIds] =useState(['c1', 'c3'])
 
+
+  const [options, setOptions] = useState({
+    title: {
+      text: "Page Views",
+    },
+    data: [ 
+      {totalViews: 20, English:10,date: '2022-12-06'},
+      {totalViews: 10,  English:5,date: '2022-12-07'},
+      {totalViews: 21,  English:11,date: '2022-12-08'},
+      {totalViews: 24,  English:3,date: '2022-12-09'},
+      {totalViews: 40, English:3, date: '2022-12-10'},
+      {totalViews: 10, English:3, date: '2022-12-11'},
+      {totalViews: 20, English:10,date: '2022-12-12'},
+      {totalViews: 10,  English:5,date: '2022-12-13'},
+      {totalViews: 21,  English:11,date: '2022-12-14'},
+      {totalViews: 24,  English:3,date: '2022-12-15'},
+      {totalViews: 40, English:3, date: '2022-12-16'},
+      {totalViews: 10, English:3, date: '2022-12-17'},
+      {totalViews: 20, English:10,date: '2022-12-18'},
+      {totalViews: 10,  English:5,date: '2022-12-19'},
+      {totalViews: 21,  English:11,date: '2022-12-20'},
+      {totalViews: 24,  English:3,date: '2022-12-21'},
+      {totalViews: 40, English:3, date: '2022-12-22'},
+      {totalViews: 10, English:3, date: '2022-12-23'},
+      {totalViews: 20, English:10,date: '2022-12-24'},
+      {totalViews: 10,  English:5,date: '2022-12-25'},
+      {totalViews: 21,  English:11,date: '2022-12-26'},
+      {totalViews: 24,  English:3,date: '2022-12-27'},
+      {totalViews: 40, English:3, date: '2022-12-28'},
+      {totalViews: 10, English:3, date: '2022-12-29'},
+      {totalViews: 20, English:10,date: '2022-12-30'},
+      {totalViews: 10,  English:5,date: '2022-12-31'},  
+      ],
+    series: [
+      {
+        type: "line",
+        xKey: "date",
+        yKey: "total",
+        yName: "Total",
+      },
+      {
+        type: "line",
+        xKey: "date",
+        yKey: "english",
+        yName: "English",
+      },
+    ],
+    zoom: {
+      enabled: true,
+  }
+  });
+
   useEffect(()=>{
     setTemplate(value.replace(/#([a-zA-Z0-9]+)/g, (match, p1) => {
       const found = emailTypes[Object.keys(emailTypes).find(et => emailTypes[et].find(e => e.text === match))];
@@ -61,6 +114,14 @@ function App() {
     const totalViewsByDate = restructureData(graphData);
     console.log(totalViewsByDate)
     setFormmatedGraphData(totalViewsByDate)
+
+    const agGraphData = restructureData2(graphData)
+    console.log("AG graph data", agGraphData)
+    const updatedOptions =produce(options,(draft)=>{
+      draft.data = agGraphData
+    })
+    setOptions(updatedOptions)
+     
   },[startDate, endDate])
 
   // Function to restructure the data as per the desired format
@@ -110,7 +171,73 @@ function App() {
 
     return result;
   }
+  
 
+  function restructureData2(courseViews){
+        
+      // Initialize an empty object to store the aggregated views
+      const aggregatedViews = {};
+
+      // Iterate through the courseViews array
+      courseViews.forEach(view => {
+          let { date, courseId } = view;
+           date = date.split("T")[0]
+           if(startDate <= date && date <=endDate)
+            { 
+                // Check if the date exists in the aggregatedViews object, if not, initialize it
+                if (!aggregatedViews[date]) {
+                    aggregatedViews[date] = {};
+                }
+
+                // Check if the courseId exists in the aggregatedViews object for the specific date, if not, initialize it
+                if (!aggregatedViews[date][courseId]) {
+                    aggregatedViews[date][courseId] = {
+                        totalViews: 0,
+                        courseName: view.courseName
+                    };
+                }
+
+                // Increment the totalViews for the specific course and date
+                aggregatedViews[date][courseId].totalViews++;
+          }
+      });
+
+      // Convert the aggregatedViews object into an array of objects
+      // const aggregatedViewsList = Object.keys(aggregatedViews).map(date => {
+      //     const courseViews = Object.values(aggregatedViews[date]);
+      //     return {
+      //         date: date,
+      //         ...courseViews.map(course => ({[course.courseName ]: course.totalViews}))
+      //     };
+      // }); 
+      // Convert the aggregatedViews object into an array of objects
+      const aggregatedViewsList = Object.keys(aggregatedViews).map(date => {
+          const courseViews = Object.values(aggregatedViews[date]);
+
+          let combinedTotal = 0;
+          let subjectViews = {};
+
+            courseViews.forEach(course => {
+              
+              // Add total views for the current course to the combined total
+              combinedTotal += course.totalViews;
+              
+              // Extract the subject name from the courseId
+              const subject = course.courseName.toLowerCase();
+              
+              // Add the subject views to the subjectViews object
+              subjectViews[subject] = course.totalViews;
+          });
+
+          return {
+              date: date,
+              total:combinedTotal,
+              ...subjectViews
+          };
+      }); 
+
+      return aggregatedViewsList
+  }
 
   return (
     <div className="App">
@@ -119,7 +246,7 @@ function App() {
         <input type="date" value={startDate} onChange={(e)=>setStartDate(e.target.value)}/>
         <input type="date" value={endDate} onChange={(e)=>setEndDate(e.target.value)}/>
       </div>
-
+      <div>
       <LineChart
         xAxis={[{ scaleType: 'point', data: Object.keys(formattedGraphData?.totalViews? formattedGraphData?.totalViews : {}) ? Object.keys(formattedGraphData?.totalViews? formattedGraphData?.totalViews : {}) : [] }]}
         series={[
@@ -145,8 +272,14 @@ function App() {
         ]}
         width={800}
         height={500}
-        grid={{ vertical: true, horizontal: true }}
+        grid={{ vertical: false, horizontal: true }}
       />
+
+      </div>
+    <div style={{height: '500', width:800}}>
+     <AgChartsReact options={options} />
+
+    </div>
 
       {/* <div style={{display:'flex', flexDirection:'row', gap: 20, padding:20}}>
         <div style={{flex:1}}> 
